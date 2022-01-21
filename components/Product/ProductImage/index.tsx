f8b96ca549173ff32e1c09b81cbc1f0d56f835c9
romualdo-ah <romualdoah@gmail.com>
2022-01-21 17:49:05 -0300

    commenting the use of LazyLoadImage

--- components/Product/ProductImage/index.tsx
+++ components/Product/ProductImage/index.tsx
@@ -11,7 +11,12 @@ interface ProductImageProps {
 export function ProductImage({ product, styles }: ProductImageProps) {
 	return (
 		<div className={`${styles} relative`}>
-			<LazyLoadImage src={product.image} alt={product.name} effect='blur' placeholderSrc="/default-image.jpg"/>
+			{/* adding LazyLoadImage component to armonize the loading of images,
+			effect: 'blur' is the effect that will be applied to the image,
+			placeholderSrc: '/default-image.jpg' is the placeholder image that will be shown while the image is loading 
+			threshold: is the distance from the bottom of the screen that the image will be loaded
+			*/}
+			<LazyLoadImage src={product.image} alt={product.name} effect='blur' placeholderSrc="/default-image.jpg" threshold="300"/>
 		</div>
 	);
 }
